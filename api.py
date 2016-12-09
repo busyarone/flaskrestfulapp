35586a64d8695384b662c745565e9a53410fd737
Arone Arokiasami <arone@dropthought.com>
2016-12-08 16:54:28 -0800

    Flask-Sqlalchemy with sqlite

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from flask import Flask,request
 from flask_restful import Resource, Api, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 api = Api(app)
 
 todos = {}
@@ -19,15 +23,38 @@ def abort_if_not_found(todo_id):
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
+parser.add_argument('id')
 
 class HelloWorld(Resource):
   def get(self):
     return {'msg':'Hello World!'}
 
 
+class Task(db.Model):
+  __tablename__ = 'Task'
+  todo_id = db.Column(db.String(20),primary_key=True)
+  task = db.Column(db.String(80))
+ 
+  
+  def __init__(self,todo_id,task):
+    self.task = task
+    self.todo_id = todo_id
+  
+  #todo_id = db.Column(db.String(20),primary_key=True)
+  #task = db.Column(db.String(80))
+
+  def __repr__(self):
+    return '<Task %r>' % self.todo_id
+
 class TodoSimple(Resource):
   def get(self,todo_id):
     return {todo_id:todos[todo_id]}
+    #print 'Here in todo simple'
+    #print 'Todo id', todo_id
+    #task = Task.query.filter_by(todo_id=todo_id).first_or_404()
+    #print task
+    #return{todo_id:'Here in todo simple'}
+    #return {todo_id:todos[todo_id]}
 
   def put(self,todo_id):
     todos[todo_id] = request.form['data']
@@ -36,34 +63,47 @@ class TodoSimple(Resource):
 
 class Todo(Resource):
   def get(self, todo_id):
-    abort_if_not_found(todo_id)
-    return TODOS[todo_id]
+    #abort_if_not_found(todo_id)
+    task = Task.query.filter_by(todo_id=todo_id).first_or_404()
+    return {todo_id:task.task}
 
   def delete(self, todo_id):
-    abort_if_not_found(todo_id)
-    del TODOS[todo_id]
+    #abort_if_not_found(todo_id)
+    task = Task.query.filter_by(todo_id=todo_id).first_or_404()
+    delete_task = Task(todo_id,task.task)
+    db.session.delete(delete_task)
+    db.session.commit()
     return '', 204
 
   def put(self, todo_id):
     args = parser.parse_args()
-    task = {'task': args['task']}
-    TODOS[todo_id] = task
+    update_task = Task(todo_id,args['task'])
+    db.session.query.filter_by(todo_id=todo_id).update(update_task)
     return task, 201
 
 
 class TodoList(Resource):
   def get(self):
-    return TODOS
+    tasks = Task.query.all()
+    task_dict = {}
+    for task in tasks:
+      task_dict[task.todo_id] = task.task
+    return task_dict
 
   def post(self):
     args = parser.parse_args()
-    #print 'args', args
-    #print TODOS.keys(),len(TODOS) 
-    #todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-    todo_id = len(TODOS) + 1
+    id = args['id']
+    #tasks = Task.query.all()
+    #todo_id = len(tasks) + 1
+    todo_id = id
     todo_id = 'todo%i' % todo_id
-    TODOS[todo_id] = {'task': args['task']}
-    return TODOS[todo_id], 201
+    new_task = Task(todo_id,args['task'])
+    db.session.add(new_task)
+    db.session.commit()
+    return_dict = {}
+    return_dict[todo_id] = args['task']
+    return return_dict
+   
 
 
 
